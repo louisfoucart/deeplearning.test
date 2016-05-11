d9a467009deb6c9894044546a54f5188e7d8fa47
louisfoucart <louis.foucart@gmail.com>
2016-05-12 00:43:03 +0200

    rename variables

--- src/main/kotlin/shazam/FirstRNN.kt
+++ src/main/kotlin/shazam/FirstRNN.kt
@@ -8,17 +8,19 @@ import org.deeplearning4j.datasets.iterator.DataSetIterator
 
 fun main(args: Array<String>) {
     val miniBatchSize = 100
-    val numPossibleLabels = 10
+    val numberOfLabels = 10
+    val numberOfExamples = 1301
     val inputPath = "/src/main/resources/shazam/data_exploded"
+
     val featureReader: SequenceRecordReader = CSVSequenceRecordReader(0, ",")
-    featureReader.initialize(NumberedFileInputSplit("$inputPath/nc13_train_feature_%d.csv", 1, 1301))
+    featureReader.initialize(NumberedFileInputSplit("$inputPath/nc13_train_feature_%d.csv", 1, numberOfExamples))
     val labelReader:SequenceRecordReader = CSVSequenceRecordReader(0, ",")
-    labelReader.initialize(NumberedFileInputSplit("$inputPath/nc13_train_label_%d.csv", 1, 1301))
+    labelReader.initialize(NumberedFileInputSplit("$inputPath/nc13_train_label_%d.csv", 1, numberOfExamples))
 
     val datasetIterator: DataSetIterator = SequenceRecordReaderDataSetIterator(
             featureReader, labelReader,
             miniBatchSize,
-            numPossibleLabels, false,
+            numberOfLabels, false,
             SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END)
 
 }
